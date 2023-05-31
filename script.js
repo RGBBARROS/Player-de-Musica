3671c51f59614f9e10077be1c3e005ff4e855807
Roger Barros <107211438+RGBBARROS@users.noreply.github.com>
2023-05-31 13:16:40 -0300

    Atualização

--- script.js
+++ script.js
@@ -1,6 +1,83 @@
 const songName = document.getElementById('music-name');
+const bandName = document.getElementById('band-name');
 const song = document.getElementById('audio');
+const cover = document.getElementById('cover');
+const play = document.getElementById('play');
+const next = document.getElementById('next');
+const previous = document.getElementById('previous');
 
-songName.innerText = 'Zóio de lula';
-song.play();
-song.pause();
+const Zóiodelula = {
+  songName: 'Zóio de lula',
+  artist: 'Charllie Brown',
+  file: 'zoio-de-lula',
+};
+
+const PescadordeIlusões = {
+  songName: 'Pescador de ilusões',
+  artist: 'O Rappa',
+  file: 'pescador-de-ilusoes',
+};
+let isPlaying = false;
+const playlist = [Zóiodelula, PescadordeIlusões];
+let index = 0;
+//song.play();
+//song.pause();
+
+
+function playSong() {
+  play.querySelector('.bi').classList.remove('bi-play-circle-fill');
+  play.querySelector('.bi').classList.add('bi-pause-circle-fill');
+  song.play();
+  isPlaying = true;
+}
+
+function pauseSong() {
+  play.querySelector('.bi').classList.add('bi-play-circle-fill');
+  play.querySelector('.bi').classList.remove('bi-pause-circle-fill');
+  song.pause();
+  isPlaying = false;
+}
+
+function playPauseDedider() {
+  if(isPlaying === true){
+    pauseSong();
+  }
+  else {
+    playSong();
+  }
+}
+
+function initializeSong(){
+  cover.src = `image/${playlist[index].file}.jpg`;
+  song.src = `songs/${playlist[index].file}.mp3`;
+  songName.innerText = playlist[index].songName;
+  bandName.innerText = playlist[index].artist;
+}
+
+function previousSong() {
+  if (index === 0) {
+    index = playlist.length -1;
+  }
+  else {
+    index = index - 1;
+  }
+  initializeSong();
+  playSong();
+}
+
+function nextSong() {
+  if (index === playlist.length -1) {
+    index = 0;
+  }
+  else {
+    index = index + 1;
+  }
+  initializeSong();
+  playSong();
+}
+
+initializeSong();
+
+play.addEventListener('click', playPauseDedider);
+previous.addEventListener('click',previousSong);
+next.addEventListener('click',nextSong);
